0a752b8c01bd781b3ba4a37c8d51cc34ff917935
mhm <mosleh@yahoo.com>
2015-03-02 09:58:19 -0500

    sd

---
+++ db_config.php
@@ -0,0 +1,18 @@
+<?php
+$host="localhost";
+$dbusername="root";
+$dbpassword="password";
+$db="taqyem";
+
+/*
+
+$basedate = new DateTime(date("Y-m-d"));
+$basedate->sub(new DateInterval('P30D'));
+
+$from_date= $basedate->format('Y-m-d');
+$to_date=date("Y-m-d");
+
+date_default_timezone_set('America/Chicago');
+$refresh_date = date('m/d/Y h:i:s a', time());
+*/
+?>
